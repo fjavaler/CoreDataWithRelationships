691de04a60c6129a40773cbed4f16ff59aea5ee6
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 14:33:22 -0600

    Initial Commit

---
+++ CoreDataWithRelationships/CoreDataWithRelationshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWithRelationshipsApp.swift
+//  CoreDataWithRelationships
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWithRelationshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
